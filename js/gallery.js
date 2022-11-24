94ddd622e9f7cf922c264a6171120982d34bc497
awheelr <austinskullfnider@gmail.com>
2022-11-23 23:04:48 -0500

    personal website

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const number = 17;
+
+  for (let i = 0; i < number; i++) {
+    const gallery = `<img class="lightbox_trigger" id="lightbox_trigger" src="./images/photo${i}.jpg">`;
+
+    const target = document.querySelector(".items");
+    target.insertAdjacentHTML("beforeend", gallery);
+  }
+});
+
+const imgs = document.querySelectorAll(".items img"); 
+
+imgs.forEach((img, id) => {
+  let currentDelay = 300;
+  img.style.animationDelay = `${currentDelay + id * 50}ms`;
+});
+
+window.onload = function () {
+const imgs = document.querySelectorAll(".items img"); 
+  imgs.forEach((img) => {
+    if (img.offsetTop < window.innerHeight) {
+      img.style.animationPlayState = "running";
+    }
+  });
+};
+
+window.addEventListener("scroll", function () {
+const imgs = document.querySelectorAll(".items img"); 
+  imgs.forEach((img) => {
+    if (img.offsetTop - img.offsetTop / 2 < window.scrollY) {
+      img.style.animationPlayState = "running";
+    }
+  });
+});
